c6aa1c36814625d97df3eeba78da5772088bff8b
Larieux <larieux.professionnelle@gmail.com>
2025-01-23 14:18:13 +0100

    hpp

--- CPP00/ex01/Contact.hpp
+++ CPP00/ex01/Contact.hpp
@@ -1,18 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlarieux <mlarieux@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/23 13:47:41 by mlarieux          #+#    #+#             */
+/*   Updated: 2025/01/23 13:58:36 by mlarieux         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef CONTACT_HPP
 # define CONTACT_HPP
+
+#include <iostream>
 
 class Contact {
 
 private:
 
-	const char	*darkest_secret;
+	std::string	first_name;
+	std::string	last_name;
+	std::string	nickname;
+	std::string	phone_number;
+	std::string	secret;
 
 public:
 
-	const char	*first_name;
-	const char	*last_name;
-	const char	*nickname;
-	const int	number;
+	Contact( void );
+	~Contact( void );
+
 
 };
 
--- CPP00/ex01/PhoneBook.hpp
+++ CPP00/ex01/PhoneBook.hpp
@@ -1,6 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlarieux <mlarieux@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/23 13:47:38 by mlarieux          #+#    #+#             */
+/*   Updated: 2025/01/23 13:57:53 by mlarieux         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PHONEBOOK_HPP
 # define PHONEBOOK_HPP
 
+#include <iostream>
 #include "Contact.hpp"
 
 class PhoneBook {
@@ -10,6 +23,9 @@ private:
 	Contact	my_contact[8];
 
 public:
+
+	PhoneBook( void );
+	~PhoneBook ( void );
 
 	Contact	getContact() const;
 	void	addContact();
