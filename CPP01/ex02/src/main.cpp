910725ae98f19870a4a1763f5ef6911a08159cb4
Larieux <larieux.professionnelle@gmail.com>
2025-03-10 17:08:24 +0100

    add ex02

--- CPP01/ex02/src/main.cpp
+++ CPP01/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mlarieux <mlarieux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 14:50:59 by mlarieux          #+#    #+#             */
-/*   Updated: 2025/03/10 15:32:53 by mlarieux         ###   ########.fr       */
+/*   Updated: 2025/03/10 17:06:39 by mlarieux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,9 @@ int main( void )
 	std::string str = "HI THIS IS BRAIN";
 	std::string* ptr = &str;
 	std::string& ref = str;
+
+	std::cout << &str << '\n' << ptr << '\n' << &ref << std::endl;
+	std::cout << str << '\n' << *ptr << '\n' << ref << std::endl;
+
+	return (0);
 }
