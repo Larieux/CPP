38f0b02062d53346f0528b20af356b97f5475089
Larieux <larieux.professionnelle@gmail.com>
2025-09-13 23:55:18 +0200

    start CPP06

--- CPP05/ex02/src/PresidentialPardonForm.cpp
+++ CPP05/ex02/src/PresidentialPardonForm.cpp
@@ -6,7 +6,7 @@
 /*   By: mlarieux <mlarieux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/30 16:31:47 by mlarieux          #+#    #+#             */
-/*   Updated: 2025/07/02 11:05:33 by mlarieux         ###   ########.fr       */
+/*   Updated: 2025/09/13 18:46:26 by mlarieux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,7 +59,7 @@ std::string	PresidentialPardonForm::getTarget( void ) const
 	return ( _target );
 }
 
-void				PresidentialPardonForm::printExecute( void ) const 
+void				PresidentialPardonForm::printExecute( void ) const
 {
 	std::cout << _target << " has been pardoned by Zaphod Beeblebrox" << std::endl;
 }
