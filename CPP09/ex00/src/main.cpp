6160c3020324241b7f321cac319be4e3de1bda01
Larieux <larieux.professionnelle@gmail.com>
2025-10-12 17:34:25 +0200

    cpp09 init

---
+++ CPP09/ex00/src/main.cpp
@@ -0,0 +1,7 @@
+#include "BitcoinExchange.hpp"
+
+int	main( void )
+{
+	// Use a map!!
+	return (0);
+}
---
+++ CPP09/ex02/src/main.cpp
@@ -0,0 +1,8 @@
+#include "PmergeMe.hpp"
+
+int main()
+{
+	// vector and deque?
+
+return (0);
+}
